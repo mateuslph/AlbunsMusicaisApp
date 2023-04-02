8a703450c6f156ae1a0320a30681e1b807a436bd
mateuslph <mlptche@gmail.com>
2023-04-02 20:52:52 -0300

    App finalizado com a exibicao do DB em uma list na tela

--- app/src/main/java/sp/senai/AlbumDAO.java
+++ app/src/main/java/sp/senai/AlbumDAO.java
@@ -2,7 +2,11 @@ package sp.senai;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AlbumDAO {
     private Conexao conexao;
@@ -19,5 +23,19 @@ public class AlbumDAO {
         values.put("ano", album.getAno());
         values.put("banda", album.getBanda());
         return banco.insert("albuns", null, values);
+    }
+
+    public List<Album> obterTodos() {
+        List<Album> albuns = new ArrayList<>();
+        Cursor cursor = banco.query("albuns", new String[]{"id", "nome", "ano", "banda"}, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            Album a = new Album();
+            a.setId(cursor.getInt(0));
+            a.setNome(cursor.getString(1));
+            a.setAno(cursor.getString(2));
+            a.setBanda(cursor.getString(3));
+            albuns.add(a);
+        }
+        return albuns;
     }
 }
--- app/src/main/java/sp/senai/CadastroActivity.java
+++ app/src/main/java/sp/senai/CadastroActivity.java
@@ -2,17 +2,30 @@ package sp.senai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class CadastroActivity extends AppCompatActivity {
 
+    private Button btnListar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        btnListar = findViewById(R.id.btnListar);
+        btnListar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CadastroActivity.this, Listagem.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void inserir(View view) {
