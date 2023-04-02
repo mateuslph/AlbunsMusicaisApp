ebd6825b1e5e043027bbe2abdb0815469aec8508
mateuslph <mlptche@gmail.com>
2023-04-01 22:10:57 -0300

    Manipulacao do Banco de Dados

--- app/src/main/java/sp/senai/CadastroActivity.java
+++ app/src/main/java/sp/senai/CadastroActivity.java
@@ -3,6 +3,9 @@ package sp.senai;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+    }
+
+    public void inserir(View view) {
+        EditText etNome = findViewById(R.id.editTextNome);
+        String nome = etNome.getText().toString();
+
+        EditText etAno = findViewById(R.id.editTextAno);
+        String ano = etAno.getText().toString();
+
+        EditText etBanda = findViewById(R.id.editTextBanda);
+        String banda = etBanda.getText().toString();
+
+        Album album = new Album(nome, ano, banda);
+        AlbumDAO dao = new AlbumDAO(this);
+
+        Long id = dao.inserir(album);
+
+        Toast.makeText(this, "Album Inserido com id " + id, Toast.LENGTH_SHORT).show();
     }
 }
